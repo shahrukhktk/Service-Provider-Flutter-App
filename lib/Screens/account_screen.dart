61968165bd8d240120cecfc5ae38d6e38c91b3ae
Shahrukh <Shahrukhktk760@gmail.com>
2020-12-17 14:17:57 +0500

    init commit 1. screen one done 2. screen two done 3. screen three

--- lib/Screens/account_screen.dart
+++ lib/Screens/account_screen.dart
@@ -57,10 +57,10 @@ class _AccountsScreenState extends State<AccountsScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text("Your Subscriptions", style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),),
+                Text("Your Subscriptions", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),),
                 GestureDetector(
                     onTap: () {},
-                    child: Text("See More", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.black,),),),
+                    child: Text("See More", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: AppColors.themecolor,),),),
               ],
             ),
           ),
@@ -68,56 +68,120 @@ class _AccountsScreenState extends State<AccountsScreen> {
           Padding(
             padding: const EdgeInsets.only(left: 12.0, top: 20),
             child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
 
-                  SizedBox(
-                    height: 150,
-                    width: 250,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Card(
-                        elevation: 2,
-                        shadowColor: Colors.black12,
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text("Start-up Package", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: AppColors.themecolor,),),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text("Subscription Free : R500", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.black38,),),
-                                ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 150,
+                      width: 250,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Card(
+                          elevation: 2,
+                          shadowColor: Colors.black12,
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text("Start-up Package", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: AppColors.themecolor,),),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text("Subscription Free : R500", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.black38,),),
+                                  ),
 
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    FlatButton(onPressed: null, child: Card(
-                                      elevation: 5,
-                                      color: AppColors.themecolor,
-                                      shadowColor: AppColors.accentcolor,
-                                      child: Container(
-                                        height: 40,
-                                        width: 100,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(25),
-                                        ),
-                                          child: Center(child: Text('Upgrade', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),),),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      FlatButton(onPressed: null, child: Card(
+                                        elevation: 5,
+                                        color: AppColors.themecolor,
+                                        shadowColor: AppColors.accentcolor,
+                                        child: Container(
+                                          height: 40,
+                                          width: 100,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(25),
+                                          ),
+                                            child: Center(child: Text('Upgrade', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),),),
+                                          ),
                                         ),
                                       ),
-                                    ),
 
-                                    SizedBox(width: 20,),
+                                      SizedBox(width: 20,),
 
-                                    Text("Details", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.black38,),),
+                                      Text("Details", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.black38,),),
 
-                                  ],
-                                )
-                              ],
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 150,
+                      width: 250,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Card(
+                          color: AppColors.accentcolor,
+                          elevation: 2,
+                          shadowColor: Colors.black12,
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text("Monthly Card", style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal, color: Colors.white,),),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text("500 mins monthly subscription", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.white,),),
+                                  ),
+
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      FlatButton(onPressed: null, child: Card(
+                                        elevation: 5,
+                                        color: Colors.white,
+                                        shadowColor: Colors.white,
+                                        child: Container(
+                                          height: 40,
+                                          width: 100,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(25),
+                                          ),
+                                          child: Center(child: Text('Activate', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black),),),
+                                        ),
+                                      ),
+                                      ),
+
+                                      SizedBox(width: 20,),
+
+                                      Text("Details", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.black38,),),
+
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
@@ -128,7 +192,21 @@ class _AccountsScreenState extends State<AccountsScreen> {
                 ],
               ),
             ),
-          )
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 12, top: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Account Setup", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text("Summary Report", style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: AppColors.themecolor,),),),
+              ],
+            ),
+          ),
 
         ],
       ),
